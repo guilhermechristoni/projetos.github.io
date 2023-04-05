15d26a791d1d873dfdf764358b5692d80a5cff78
Guilherme Garcia Christoni <92758108+guilhermechristoni@users.noreply.github.com>
2023-04-05 17:33:13 -0300

    Update sounds.js

--- js/sounds.js
+++ js/sounds.js
@@ -1,8 +1,8 @@
 export const sons = {
-    bgAudioRain : new Audio("./sounds/chuva.mp4"),
-    bgAudioForest : new Audio("./sounds/floresta.mp4"),
-    bgAudioCoffeShop : new Audio("./sounds/cafeteria.mp4"),
-    bgAudioFireplace : new Audio("./sounds/lareira.mp4"),
+    bgAudioRain : new Audio("https://user-images.githubusercontent.com/92758108/230202806-0d41744f-ceac-4a1b-b959-323c4cc5c9ec.mp4"),
+    bgAudioForest : new Audio("https://user-images.githubusercontent.com/92758108/230202813-00c6de89-98ca-4ec0-8bb7-e8692cb6e0f0.mp4"),
+    bgAudioCoffeShop : new Audio("https://user-images.githubusercontent.com/92758108/230202785-fb32d20b-6433-439e-a29e-8f07cc6debe7.mp4"),
+    bgAudioFireplace : new Audio("https://user-images.githubusercontent.com/92758108/230202826-7419aff7-cc93-41e7-a171-6d67df00490f.mp4"),
 
     //Elementos Volume
     controlVolTree: document.querySelector(".input-vol-tree"),
@@ -10,8 +10,8 @@ export const sons = {
     controlVolCoffe: document.querySelector(".input-vol-coffe"),
     controlVolFire: document.querySelector(".input-vol-fire"),
 
-    buttonPressAudio : new Audio("./sounds/audios_button-press.mp3"),
-    kitchenTimer : new Audio("./sounds/audios_kichen-timer.mp3"),
+    buttonPressAudio : new Audio("https://user-images.githubusercontent.com/92758108/230203909-8f812503-4bd7-4dcd-960f-3573b4f9645f.mp4"),
+    kitchenTimer : new Audio("https://user-images.githubusercontent.com/92758108/230203873-11c1517b-fcad-49c4-9dce-fef397453779.mp4"),
 
 
     controlVolumeForest : document.querySelector(".input-vol-tree"),
